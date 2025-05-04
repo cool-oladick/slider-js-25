49b0fe32ab40007b0cebb7063977a8c0830fba31
miwanoff <marina.iva.noffa@gmail.com>
2025-05-04 11:28:52 +0300

    Add swapImg

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let slides = ["images/1.jpg", "images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg",];
+let slides = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg",
+];
+
+let i = 0;
+
+let slider = document.getElementById("slider");
+
+function swapImg() {
+  if (i < slides.length - 1) {
+    i++;
+    console.log(i);
+  } else {
+    i = 0;
+  }
+  slider.setAttribute("src", slides[i]);
+}
