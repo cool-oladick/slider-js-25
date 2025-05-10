5bd7f9871f8d5e931165b449caa8d6ddb86c020a
cool-oladick <razborka.alekzs16@gmail.com>
2025-05-10 09:01:08 +0200

    new photo and prev

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-let slides = ["images/1.jpg", "images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg",];
+let slides = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+];
+
+let i = 0;
+
+let slider = document.getElementById("slider");
+
+function next() {
+    if (i < slides.length - 1) {
+        i++;
+        console.log(i);
+    } else {
+        i = 0;
+    }
+    slider.setAttribute("src", slides[i]);
+}
+
+function prev() {
+    if (i > 0) {
+        i--;
+        console.log(i);
+    } else {
+        i = slides.length - 1;
+    }
+    slider.setAttribute("src", slides[i]);
+}
